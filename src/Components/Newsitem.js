9062c3d4428bf711e713e58b6b4b3328c3c0d89d
Naman <namanvarshneyofficial@gmail.com>
2021-10-06 02:02:27 +0530

    Corrected the badge & Improved card

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        let { title, description, imageUrl, newsUrl, date, source } = this.props;
         return (
             <div className="card" style={{ backgroundColor: "#282828" }}>
-                <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: "90%", zIndex: "1" }}>
-                    {source}
-                </span>
+                <span class={`position-absolute top-0 translate-middle badge rounded-pill bg-${"primary"}`} style={{ left: "90%", zIndex: "1" }}>{source}</span>
                 <img src={imageUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
+                    <p className="card-text"><small className="text-muted">{new Date(date).toGMTString()}</small></p>
                     <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
                 </div>
             </div>
