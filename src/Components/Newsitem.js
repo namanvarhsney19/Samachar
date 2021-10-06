2604f1a9de25188023b182fda6991ab2ca1a2fa4
Naman <namanvarshneyofficial@gmail.com>
2021-10-06 22:42:03 +0530

    Corrected Sorce Badge

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -5,9 +5,12 @@ export class Newsitem extends Component {
         let { title, description, imageUrl, newsUrl, date, source, category } = this.props;
         let badgeColor = { "science": "primary", "entertainment": "danger", "technology": "primary", "business": "secondary", "health": "danger", "sports": "warning", "general": "primary" }
         return (
-            <div className="card" style={{ backgroundColor: "#282828" }}>
-                <span className={`position-absolute top-0 translate-middle badge rounded-pill bg-${badgeColor[category]}`} style={{ left: "80%", zIndex: "1" }}>{source}</span>
+            <div className="card my-1" style={{ backgroundColor: "#282828" }}>
+
+                <span className={`position-absolute badge rounded-pill bg-${badgeColor[category]}`} style={{ zIndex: "1", right: "0" }}>{source}</span>
+
                 <img src={imageUrl} className="card-img-top" alt="..." />
+
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
