9401b568a6bd55176beec5d989f6a1d61736892a
Naman <namanvarshneyofficial@gmail.com>
2021-10-15 20:32:24 +0530

    Added transition

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import "./Newsitem.css"
 const Newsitem = (props) => {
     let { title, description, imageUrl, newsUrl, date, source, category, mode } = props;
     let badgeColor = { "science": "primary", "entertainment": "danger", "technology": "primary", "business": "secondary", "health": "danger", "sports": "warning", "general": "primary" }
     return (
-        <div className="card my-1" style={{ backgroundColor: mode === "dark" ? "#282828" : "white" }} >
+        <div className="card my-3" style={{ backgroundColor: mode === "dark" ? "#282828" : "white" }} >
             {/*  style={{ backgroundColor: "#282828" }} */}
             <span className={`position-absolute badge rounded-pill bg-${badgeColor[category]}`} style={{ zIndex: "1", right: "0" }}>{source}</span>
 
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img src={imageUrl} className="card-img-top image" alt="..." />
 
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
